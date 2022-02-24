859c32a8085754ec033c199f1fd5f1ab756c1cb8
Danger <51889207+DangerDan88@users.noreply.github.com>
2022-02-23 20:06:41 -0700

    little tweaking tryna get the form submisson using just JS

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 // Add JS to get form info from submission of from and display on page
+// find a plain js way to get the form info to get stored into localStorage
+document.addEventListener("DOMContentLoaded", function (event) {
+  let button = document.getElementById("button");
+  let msg = document.getElementById("item");
 
-console.log("test");
+  button.addEventListener("click", function () {
+    console.log("test");
+  });
+});
