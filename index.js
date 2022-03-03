0a551e0f92d668fc07bb225581c20f4cbbd26246
Danger <51889207+DangerDan88@users.noreply.github.com>
2022-03-02 20:10:54 -0700

    got form to submit added prevent default

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 // Add JS to get form info from submission of from and display on page
 // find a plain js way to get the form info to get stored into localStorage
-document.addEventListener("DOMContentLoaded", function (event) {
-  let button = document.getElementById("button");
-  let msg = document.getElementById("item");
+// run jquery document.ready in meantime so we can test this
 
-  button.addEventListener("click", function () {
-    console.log("test");
-  });
+$(document).ready(function () {
+  let button = document.getElementById("button");
+  button.addEventListener("click", displayText);
+  console.log("test");
+  function displayText(event) {
+    event.preventDefault();
+    let msg = document.getElementById("item").value;
+    console.log(msg);
+  }
+
+  displayText();
 });
