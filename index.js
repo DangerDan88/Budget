223b0968aca57bf7b563ffae07f958e9c0c05a5f
Danger <51889207+DangerDan88@users.noreply.github.com>
2022-03-05 17:23:28 -0700

    did a little styling need to figure out why appending is breaking submisson

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 /// got the items to set in local storage need to see if I can get them to stay on page reload
 // concatenate the items into list items when submitting
 // style it up
+// see if this works with plain javascript take out the jquery
 $(document).ready(function () {
   let button = document.getElementById("button");
   button.addEventListener("click", displayText);
-  console.log("test");
+
   function displayText(event) {
     event.preventDefault();
     let msg = document.getElementById("item").value;
@@ -13,15 +14,16 @@ $(document).ready(function () {
       style: "currency",
       currency: "USD",
     });
-    localStorage.setItem("item", msg);
-    localStorage.setItem("amount", dollar);
-    document.getElementById("formData").innerHTML =
-      msg + "  " + numbUSD.format(dollar);
-    //  document.getElementById("formData").innerHTML = Number(
-    //    localStorage.getItem("amount")
-    //  );
-    //   localStorage.getItem("item") +
-    //   "  " +
+    let listItem = document.createElement("li");
+    let whatIsThis = (document.getElementById("formData").innerHTML =
+      listItem.appendChild(
+        document.createTextNode(msg + " " + numbUSD.format(dollar))
+      ));
+    console.log(whatIsThis);
+    // why is this showing as an object text but in console its just a string?
   }
   displayText();
 });
+
+//localStorage.setItem("item", msg);
+//localStorage.setItem("amount", dollar);
