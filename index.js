40025a3650e24a65ba34f69e6027f08e8b77e1f4
Danger <51889207+DangerDan88@users.noreply.github.com>
2022-03-07 17:58:38 -0700

    removed comments and made a few style changes need to make function  to fetch the local storage items

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /// got the items to set in local storage need to see if I can get them to stay on page reload
-// concatenate the items into list items when submitting
 // style it up
 // see if this works with plain javascript take out the jquery
+
 $(document).ready(function () {
   let button = document.getElementById("button");
   button.addEventListener("click", displayText);
@@ -14,15 +14,12 @@ $(document).ready(function () {
       style: "currency",
       currency: "USD",
     });
-    // let listItem = document.createElement("li");
+
     let listItem = document.getElementById("formData");
     listItem.innerHTML +=
       "<li>" + msg + "  " + numbUSD.format(dollar) + "<li/>";
-    // why is this showing as an object text but in console its just a string?
-    // could I try setAttribute instead for the form?
+    // localStorage.setItem("item", msg);
+    // localStorage.setItem("amount", numbUSD.format(dollar));
   }
   displayText();
 });
-
-//localStorage.setItem("item", msg);
-//localStorage.setItem("amount", dollar);
