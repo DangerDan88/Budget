be9a7197eb954b9596d007c2073fec20711f4432
Danger <51889207+DangerDan88@users.noreply.github.com>
2022-03-11 20:04:09 -0700

    got the data from local storage to actaully show instead of undefined need function to loop array of data to display to page

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-/// got the items to set in local storage need to see if I can get them to stay on page reload
-// style it up
 // see if this works with plain javascript take out the jquery
-
+// make an array to store the local storage items then I can loop them
+// need to make function to push to array of local data so I can loop all the items and display them on page
 $(document).ready(function () {
   let storageArea = document.getElementById("storageArea");
   storageArea.addEventListener("load", getLocalData);
+
   // this function display the items already in local storage
+  // need to make local storage array and loop over to display all the items
+  let localArray = [];
+
   function getLocalData() {
-    let storageItem = localStorage.getItem("amount");
-    // document.getElementById("test").innerHTML = user;
-    console.log(storageItem);
+    let storageItem = localStorage.getItem("item");
+    let itemPrice = localStorage.getItem("amount");
+    // this grabs what is already in local storage and push it into the array
+    localArray.push(storageItem + itemPrice);
+    document.getElementById("storageArea").innerHTML =
+      storageItem + "  " + itemPrice;
+
+    console.log(storageItem + itemPrice);
+    console.log(localArray);
   }
   getLocalData();
 
@@ -28,8 +37,15 @@ $(document).ready(function () {
     let listItem = document.getElementById("formData");
     listItem.innerHTML +=
       "<li>" + msg + "  " + numbUSD.format(dollar) + "<li/>";
+    // this sets item in local storage
     localStorage.setItem("item", msg);
     localStorage.setItem("amount", numbUSD.format(dollar));
+    // here we regrab from local storage to push to the array for local storage
+    let storageItem = localStorage.getItem("item");
+    let itemPrice = localStorage.getItem("amount");
+    localArray.push(storageItem + itemPrice);
+
+    console.log(msg + numbUSD.format(dollar));
   }
   displayText();
 });
